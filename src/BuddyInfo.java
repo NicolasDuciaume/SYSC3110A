866a4a0710403418cd0fcd1548b363fbdccd643c
duciaume <42221076+duciaume@users.noreply.github.com>
2020-10-02 11:45:48 -0400

    removed what I added

---
+++ src/BuddyInfo.java
@@ -0,0 +1,32 @@
+public class BuddyInfo {
+
+    public BuddyInfo(String n, String a, String p)
+    {
+        name = n;
+        address = a;
+        phoneNumber = p;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
+    public String getAddress() {
+        return address;
+    }
+
+    private String address;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    private String phoneNumber;
+
+    public static void main(String[] args) {
+
+    }
+}
